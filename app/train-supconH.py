3fb96d460dcbe0f000aac22543f926b9d1d24cd6
Yichuan <1758783038@qq.com>
2024-02-07 16:13:31 -0500

    add src.

--- app/train-supconH.py
+++ app/train-supconH.py
@@ -2,13 +2,13 @@ import torch
 import time
 import os
 import pickle
-from CLEAN.dataloader import *
-from CLEAN.model import *
-from CLEAN.utils import *
-from CLEAN.losses import SupConHardLoss
+from src.CLEAN.dataloader import *
+from src.CLEAN.model import *
+from src.CLEAN.utils import *
+from src.CLEAN.losses import SupConHardLoss
 import torch.nn as nn
 import argparse
-from CLEAN.distance_map import get_dist_map
+from src.CLEAN.distance_map import get_dist_map
 
 
 def parse():
